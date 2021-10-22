d4297416d9e32cb341b5f23aa5eaf3c8d3944ceb
Ahyeon Jeon <ahyeon@Ahyeons-MBP.attlocal.net>
2021-10-22 14:54:24 -0700

    update deposit method

--- BankAccount.py
+++ BankAccount.py
@@ -1,6 +1,12 @@
 class BankAccount:
-    def __init__(self, full_name, account_number, balance):
+    def __init__(self, full_name, account_number, balance=0):
         self.name = full_name
         self.acc_num = account_number
         self.balance = balance
- 
+    
+    def deposit(self, amount):
+        self.balance += amount
+        print(f'Amount deposited: ${amount} new balance: ${self.balance}')
+   
+ahyeon_acc = BankAccount("Ahyeon Jeon", "12345678", 30)
+ahyeon_acc.deposit(50)
