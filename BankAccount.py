95099a8404da1e43a5c3aefce5a183506359bf35
Ahyeon Jeon <ahyeon@Ahyeons-MBP.attlocal.net>
2021-10-22 14:02:51 -0700

    update attributes

---
+++ BankAccount.py
@@ -0,0 +1,6 @@
+class BankAccount:
+    def __init__(self, full_name, account_number, balance):
+        self.name = full_name
+        self.acc_num = account_number
+        self.balance = balance
+ 
