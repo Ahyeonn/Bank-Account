4cfdadfd832c529cdd7ac75822868cc2f3b389a0
Ahyeon Jeon <ahyeon@Ahyeons-MBP.attlocal.net>
2021-10-23 00:29:22 -0700

    update methods

--- BankAccount.py
+++ BankAccount.py
@@ -6,7 +6,35 @@ class BankAccount:
     
     def deposit(self, amount):
         self.balance += amount
-        print(f'Amount deposited: ${amount} new balance: ${self.balance}')
-   
-ahyeon_acc = BankAccount("Ahyeon Jeon", "12345678", 30)
-ahyeon_acc.deposit(50)
+        print(f'\nAmount deposited: ${amount} new balance: ${self.balance}\n')
+    
+    def withdraw(self, amount):
+        self.balance -= amount
+        if amount > self.balance:
+            print("Insufficient funds!!!")
+            print("Overdraft fee of $10 will be applied to your account.\n")
+            self.balance += 10
+            print(f'Amount withdrawn: ${amount} new balance: ${self.balance}')
+        else:
+            print(f'Amount withdrawn: ${amount} new balance: ${self.balance}')
+    
+    def get_balance(self):
+        print(f'Account balance: ${self.balance}\n')
+        return self.balance
+
+    def add_interest(self):
+        Interest = self.balance * 0.00083
+        self.balance += Interest
+
+    def print_statement(self):
+        print(self.name)
+        print(f'Account No. :{self.acc_num}')
+        print(f'Balance: {self.balance}\n')
+
+mitchell_acc = BankAccount("Mitchell", "3141592")
+mitchell_acc.deposit(400000)
+mitchell_acc.print_statement()
+mitchell_acc.add_interest()
+mitchell_acc.print_statement()
+mitchell_acc.withdraw(150)
+mitchell_acc.print_statement()
