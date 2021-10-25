12dc075e17e2a7723080002b54c805d995498766
Ahyeon Jeon <ahyeon@Ahyeons-MBP.attlocal.net>
2021-10-25 12:18:09 -0700

    update methods

--- BankAccount.py
+++ BankAccount.py
@@ -13,28 +13,29 @@ class BankAccount:
         if amount > self.balance:
             print("Insufficient funds!!!")
             print("Overdraft fee of $10 will be applied to your account.\n")
-            self.balance += 10
-            print(f'Amount withdrawn: ${amount} new balance: ${self.balance}')
+            self.balance -= 10
+            print(f'Amount withdrawn: ${amount} new balance: ${self.balance}\n')
         else:
-            print(f'Amount withdrawn: ${amount} new balance: ${self.balance}')
+            print(f'Amount withdrawn: ${amount} new balance: ${self.balance}\n')
     
     def get_balance(self):
         print(f'Account balance: ${self.balance}\n')
         return self.balance
 
     def add_interest(self):
-        Interest = self.balance * 0.00083
-        self.balance += Interest
+        self.balance += self.balance * 0.00083
+
+    def print_hidden_acc(self): #Need to have self as a parameter when it is inside the Class funciton
+        new_acc = "*" * (len(self.acc_num) - 4) #append is for array, string is +=, -4 means subtracts 4 * from the len(acc.num) which is 7
+        acc_numbers = list(self.acc_num)[-4:]
+        # import pdb;pdb.set_trace() #for debugging. only works on global scopes
+        for num in acc_numbers:
+            new_acc += num
+        return new_acc
 
     def print_statement(self):
         print(self.name)
-        print(f'Account No. :{self.acc_num}')
-        print(f'Balance: {self.balance}\n')
+        print(f'Account No. :{self.print_hidden_acc()}')
+        self.get_balance()
 
-mitchell_acc = BankAccount("Mitchell", "3141592")
-mitchell_acc.deposit(400000)
-mitchell_acc.print_statement()
-mitchell_acc.add_interest()
-mitchell_acc.print_statement()
-mitchell_acc.withdraw(150)
-mitchell_acc.print_statement()
+
